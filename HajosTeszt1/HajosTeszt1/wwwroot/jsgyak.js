0dd07d304e52c6b2415686e3134c9d0ab10444fc
Msoffice12861@office365svip.tw <Msoffice12861@office365svip.tw>
2021-03-19 23:05:25 +0100

    push

---
+++ HajosTeszt1/HajosTeszt1/wwwroot/jsgyak.js
@@ -0,0 +1,25 @@
+﻿var faktoriálisR = (n) => {
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        return n * faktoriálisR(n - 1)
+    }
+}
+
+let ide = document.getElementById("pascal")
+console.log("betöltődött")
+
+for (var s = 0; s < 10; s++) {
+    let sor = document.createElement("div");
+    sor.classList.add("sor");
+    ide.appendChild(sor);
+    for (var o = 0; o <= s; o++) {
+
+        let újSzám = document.createElement("div")
+        újSzám.classList.add("elem")
+        újSzám.innerText = faktoriálisR(s) / (faktoriálisR(o) * (faktoriálisR(s - o)));
+        sor.appendChild(újSzám);
+
+
+    }
+}
