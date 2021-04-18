e7b59f24dbba664936d3b0d61c8b8116f19f62f7
Msoffice12861@office365svip.tw <Msoffice12861@office365svip.tw>
2021-04-18 23:24:54 +0200

    Commit

---
+++ HajosTeszt1/HajosTeszt1/Controllers/TesztController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HajosTeszt1.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class TesztController : Controller
+    {
+        [HttpGet]
+        [Route("corvinus/szerverido")]
+        public IActionResult M1()
+        {
+            string pontosIdő = DateTime.Now.ToShortTimeString();
+
+            return new ContentResult
+            {
+                ContentType = System.Net.Mime.MediaTypeNames.Text.Plain, //"text/plain"
+                Content = pontosIdő
+            };
+        }
+
+    }
+}
--- HajosTeszt1/HajosTeszt1/Startup.cs
+++ HajosTeszt1/HajosTeszt1/Startup.cs
@@ -16,9 +16,9 @@ namespace HajosTeszt1
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -40,6 +40,12 @@ namespace HajosTeszt1
             //        await context.Response.WriteAsync("Hello World!");
             //    });
             //});
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
